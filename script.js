b8704f1c9df061fe1b0e0c2c4d3fa0b7f5603edc
Abhijeet Mishra <abhijitmishra3089@gmail.com>
2026-06-06 18:44:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,23 @@
 const input=document.getElementById("fontsize");
 const color=document.getElementById("fontcolor");
 const form = document.querySelector("form");
+window.onload = function () {
+  const cookies = document.cookie.split("; ");
+
+  cookies.forEach(cookie => {
+    const [key, value] = cookie.split("=");
+
+    if (key === "fontsize") {
+      fontSize.value = value;
+      document.body.style.fontSize = value + "px";
+    }
+
+    if (key === "fontcolor") {
+      fontColor.value = value;
+      document.body.style.color = value;
+    }
+  });
+};
 form.addEventListener('submit',(e)=>{
 	e.preventDefault();
     document.cookie = `fontsize=${input.value}`;
