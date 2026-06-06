4a4287e8afa104e0c0d47658092c84d630229572
Abhijeet Mishra <abhijitmishra3089@gmail.com>
2026-06-06 18:53:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-//your JS code here. If required.
-const input=document.getElementById("fontsize");
-const color=document.getElementById("fontcolor");
 const form = document.querySelector("form");
+const fontSize = document.getElementById("fontsize");
+const fontColor = document.getElementById("fontcolor");
 window.onload = function () {
   const cookies = document.cookie.split("; ");
   cookies.forEach(cookie => {
     const [key, value] = cookie.split("=");
-    if (key === "input") {
-      input.value = value;
-      document.body.style.input = value + "px";
+    if (key === "fontsize") {
+      fontSize.value = value;
+      document.body.style.fontSize = value + "px";
     }
-    if (key === "color") {
-      color.value = value;
+    if (key === "fontcolor") {
+      fontColor.value = value;
       document.body.style.color = value;
     }
   });
 };
-form.addEventListener('submit',(e)=>{
-	e.preventDefault();
-    document.cookie = `input=${input.value}`;
-    document.cookie = `color=${color.value}`;
-	document.body.style.input = input.value + "px";
-    document.body.style.color = color.value;
-})
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  document.cookie = `fontsize=${fontSize.value}`;
+  document.cookie = `fontcolor=${fontColor.value}`;
+  document.body.style.fontSize = fontSize.value + "px";
+  document.body.style.color = fontColor.value;
+});
