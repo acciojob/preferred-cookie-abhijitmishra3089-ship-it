cf8509ef2cb4b27e5513d118cdcdabbc53f955ad
Abhijeet Mishra <abhijitmishra3089@gmail.com>
2026-06-06 18:36:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const input=document.getElementById("fontsize");
+const color=document.getElementById("fontcolor");
+const form = document.querySelector("form");
+form.addEventListener('click',(e)=>{
+	e.preventDefault();
+    document.cookie = `fontsize=${input.value}`;
+    document.cookie = `fontcolor=${color.value}`;
+})
